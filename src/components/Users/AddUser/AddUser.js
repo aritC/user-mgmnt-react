7b673964e27d6bdef85e4de03d5fdc6bd9cc26cf
Arit Chanda <aritchanda1234@gmail.com>
2023-06-04 20:32:05 -0700

    used useRef Hook for input form

--- src/components/Users/AddUser/AddUser.js
+++ src/components/Users/AddUser/AddUser.js
@@ -1,24 +1,16 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import Card from "../../UI/Card/Card";
 import classes from "./AddUser.module.css";
 import Modal from "../../UI/Modal/Modal";
 import Button from "../../UI/Button/Button";
 
 export default function AddUser(props) {
-  const [name, setName] = useState("");
+  const nameRef = useRef();
+  const ageRef = useRef();
+
   const [showModal, setShowModal] = useState(false);
   const [modalTitle, setModalTitle] = useState("");
   const [modalMessage, setModalMessage] = useState("");
-
-  const [age, setAge] = useState("");
-
-  const handleInput = (e) => {
-    if (e.target.id === "name") {
-      setName(e.target.value);
-    } else if (e.target.id === "age") {
-      setAge(e.target.value);
-    }
-  };
 
   const setModalContent = (title, message) => {
     setModalTitle(title);
@@ -27,22 +19,23 @@ export default function AddUser(props) {
 
   const addUser = (e) => {
     e.preventDefault();
+    const inputName = nameRef.current.value;
+    const inputAge = ageRef.current.value;
     let error = false;
-    if (name.trim().length === 0 || age.trim().length === 0) {
+    if (inputName.trim().length === 0 || inputAge.trim().length === 0) {
       setModalContent("Error", "Name and Age cannot be an empty.");
       error = true;
-    } else if (+age < 1) {
+    } else if (+inputAge < 1) {
       setModalContent("Error", "Age has to be > 1.");
       error = true;
     }
 
     if (!error) {
-      props.addUser({ name, age });
+      props.addUser({ name: inputName, age: inputAge });
       setModalContent("Success", "User was added successfully!");
-      setName("");
-      setAge("");
+      nameRef.current.value = "";
+      ageRef.current.value = "";
     }
-
     setShowModal(true);
   };
 
@@ -63,9 +56,21 @@ export default function AddUser(props) {
       <Card className={classes.input}>
         <form onSubmit={addUser}>
           <label htmlFor="name">Name</label>
-          <input type="text" id="name" value={name} onChange={handleInput} />
+          <input
+            type="text"
+            id="name"
+            ref={nameRef}
+            // value={name}
+            // onChange={handleInput}
+          />
           <label htmlFor="age">Age</label>
-          <input type="number" id="age" value={age} onChange={handleInput} />
+          <input
+            type="number"
+            id="age"
+            ref={ageRef}
+            // value={age}
+            // onChange={handleInput}
+          />
           <Button type="submit">Add User</Button>
         </form>
       </Card>
